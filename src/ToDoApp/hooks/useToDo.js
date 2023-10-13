cc27279a3d9dc0ebb64cde48bde48462e966ca7a
AlainPrevot <alain.k.prevot@gmail.com>
2023-10-13 17:43:29 -0300

    Inicio estilos

---
+++ src/ToDoApp/hooks/useToDo.js
@@ -0,0 +1,51 @@
+import { useEffect, useReducer } from 'react';
+import { ToDoReducer } from '../reducer/ToDoReducer';
+
+const init = () => {
+    return JSON.parse(localStorage.getItem('todos')) || [];
+}
+
+export const useToDo = () => {
+  
+    const [ todos, dispatch ] = useReducer( ToDoReducer, [], init );
+
+    useEffect(() => {
+      localStorage.setItem('todos', JSON.stringify( todos ) );
+    }, [todos])
+    
+
+    const handleNewTodo = ( todo ) => {
+        const action = {
+            type: '[TODO] Add Todo',
+            payload: todo
+        }
+
+        dispatch( action );
+    }
+
+    const handleDeleteTodo = ( id ) => {
+        dispatch({
+            type: '[TODO] Remove Todo',
+            payload: id
+        });
+    }
+
+    const handleToggleTodo = ( id ) => {
+        dispatch({
+            type: '[TODO] Toggle Todo',
+            payload: id
+        });
+    }
+
+    return {
+        todos,
+
+        todosCount: todos.length,
+        pendingTodosCount: todos.filter(todo=> !todo.done).length,
+
+        handleNewTodo,
+        handleDeleteTodo,
+        handleToggleTodo,
+    }
+
+}
---
+++ src/ToDoApp/pages/ToDoPage.jsx
@@ -0,0 +1,38 @@
+import { TodoAdd, TodoList } from "../components";
+import { useToDo } from "../hooks";
+
+export const ToDoPage = () => {
+  const {
+    todos,
+    todosCount,
+    pendingTodosCount,
+    handleDeleteTodo,
+    handleToggleTodo,
+    handleNewTodo,
+  } = useToDo();
+
+  return (
+    <>
+      <h1>
+        TodoApp: {todosCount}, <small>pendientes: {pendingTodosCount}</small>{" "}
+      </h1>
+      <hr />
+
+      <div className="row">
+        <div className="col-7">
+          <TodoList
+            todos={todos}
+            onDeleteTodo={handleDeleteTodo}
+            onToggleTodo={handleToggleTodo}
+          />
+        </div>
+
+        <div className="col-5">
+          <h4>Agregar TODO</h4>
+          <hr />
+          <TodoAdd onNewTodo={handleNewTodo} />
+        </div>
+      </div>
+    </>
+  );
+};
